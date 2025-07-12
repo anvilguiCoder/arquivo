5ad276555f31eae7f5a6896fde387f21dc7ebd3b
anvilguiCoder <anvilgui.code@gmail.com>
2025-07-12 14:56:40 -0300

    Corrige nova importação e inicialização do admin

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from fpdf import FPDF
 from utils import validar_cpf, validar_data
 from datetime import datetime
 from werkzeug.security import check_password_hash  # certifique-se de importar isso
-from scripts_usuarios.inicializar import inicializar_admin
+from inicializar import inicializar_admin
 
 load_dotenv()
 
